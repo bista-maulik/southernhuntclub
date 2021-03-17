f57cb51b0d348d5d3df624e827c0b6d055f7c113
ravi.kikani <ravi.kikani@bistasolutions.com>
2021-03-17 16:07:46 +0530

    [ADD] Added a module of company set in default in master data and added new rules and groups and menu of industry.

---
+++ bista_user_company_set/models/__init__.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Bista Solutions Pvt. Ltd
+#    Copyright (C) 2021 (http://www.bistasolutions.com)
+#
+##############################################################################
+
+from . import product
+from . import res_partner
+from . import product_pricelist
